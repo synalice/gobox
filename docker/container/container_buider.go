62c9e3645f4892877ef7395a62812bebc556f0f4
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2022-05-28 19:38:14 +0300

    feat: implement builder pattern for container creation
    This is not really a builder patter, but I was using it as a reference here

---
+++ docker/container/container_buider.go
@@ -0,0 +1,64 @@
+package container
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+
+	"github.com/synalice/gobox/docker"
+	"github.com/synalice/gobox/docker/config"
+)
+
+type Builder struct {
+	controller *docker.Controller
+	config     *config.Config
+	container  *Container
+}
+
+func NewContainerBuilder(controller *docker.Controller, config *config.Config) *Builder {
+	return &Builder{
+		controller: controller,
+		config:     config,
+		container:  &Container{},
+	}
+}
+
+func (b *Builder) createContainer() error {
+	resp, err := b.controller.Cli.ContainerCreate(
+		context.Background(),
+		&b.config.ContainerConfig,
+		&b.config.HostConfig,
+		&b.config.NetworkingConfig,
+		&b.config.Platform,
+		b.generateUUIDName(),
+	)
+	if err != nil {
+		return fmt.Errorf("error building container: %w", err)
+	}
+
+	b.container.ID = resp.ID
+
+	return nil
+}
+
+func (b *Builder) setTimeLimit(timeLimit time.Duration) *Builder {
+	b.container.TimeLimit = timeLimit
+	return b
+}
+
+func (b *Builder) generateUUIDName() (containerName string) {
+	return "gobox" + "-" + b.config.ContainerConfig.Image + "-" + uuid.NewString()
+}
+
+func (b *Builder) Build() (*Container, error) {
+	err := b.createContainer()
+	if err != nil {
+		return nil, fmt.Errorf("couldn't create container: %w", err)
+	}
+
+	b.setTimeLimit(b.config.TimeLimit)
+
+	return b.container, nil
+}
