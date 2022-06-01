14d854224be4efbb50673a60d44211013f270e2c
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2022-06-01 16:28:50 +0300

    docs (doc.go): add import comment

--- docker/config/doc.go
+++ docker/config/doc.go
@@ -2,4 +2,4 @@
 Package config contains everything necessary to build a config that can be later
 used to build a docker container from it.
 */
-package config
+package config // import "github.com/synalice/gobox/docker/config"
--- docker/container/doc.go
+++ docker/container/doc.go
@@ -1,4 +1,4 @@
 /*
 Package container is used for creating docker containers using a builder pattern
 */
-package container
+package container // import "github.com/synalice/gobox/docker/container"
